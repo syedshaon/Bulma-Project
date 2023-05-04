f5c793a29469c5a2c5b9ab4e50e3f8b40b66c89c
Mashiur Rahman <22392318+syedshaon@users.noreply.github.com>
2023-05-04 12:19:22 +0600

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,27 @@
+// import "normalize.css";
+// Will be using reset css through scss
+
+//  TO DISABLE DONT IMPORT * AS bOOTSTRAP ALSO DON'T IMPORT BOOTSTRAP IN STYLE.SCSS FILE
+/* import "bootstrap"; */
+
+import "./style.scss";
+// Import all of Bootstrap's JS
+import * as bootstrap from "bootstrap";
+// or, specify which plugins you need:
+// import { Tooltip, Toast, Popover } from 'bootstrap'
+
+/* Following is required for Bulma CSS Hamburger Menu */
+
+const el = document.querySelector(".navbar-burger");
+
+el.addEventListener("click", () => {
+  const targetExpand = document.getElementById("navbarBasicExample");
+
+  // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
+  el.classList.toggle("is-active");
+  targetExpand.classList.toggle("is-active");
+});
+
+/* Bulma CSS Hamburger Menu Ends */
+
+/* npx kill-port 3000 */
